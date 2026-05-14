56e0b4defe3080bd6be1636c7fd630ba13dc78c2
ShathaFaisal <shdhy4662@gmail.com>
2026-05-14 19:37:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-/* Welcome Message */
+
 function showMessage() {
 
     alert("Welcome to Shatha's CV Website!");
 
 }
 
-/* Contact Message */
 function contactMessage() {
 
     alert("Thank you for visiting my CV website!");
