54b0a75de4dee9bb299fdd928df906442f0508ba
ShathaFaisal <shdhy4662@gmail.com>
2026-05-14 19:23:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Welcome Message */
+function showMessage() {
+
+    alert("Welcome to Shatha's CV Website!");
+
+}
+
+/* Contact Message */
+function contactMessage() {
+
+    alert("Thank you for visiting my CV website!");
+
+}
